25651f0205fb563d538e8be820841ce228bd647f
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-03-11 18:35:26 -0800

    Update covid_tracking

--- covid_tracking.py
+++ covid_tracking.py
@@ -3,3 +3,19 @@
 # Bashar Q Abduljaleel
 # 03.05.23
 # Group Project on The COVID Tracking https://api.covidtracking.com/v1/us/daily.csv
+
+import mysql.connector
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#connect to db
+def connect_to_sql():
+    conn = mysql.connector.connect(user='root', password='',
+                                   host='127.0.0.1', database='CovidTracking')
+    return conn
+
+#create table
+tables = pd.read_csv('https://api.covidtracking.com/v1/us/daily.csv')
+
