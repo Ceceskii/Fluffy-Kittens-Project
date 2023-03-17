e882d7f51f548e1014f8fc705ce67f0c0f12c552
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-03-16 19:12:15 -0700

    update covid_tracking

--- covid_tracking.py
+++ covid_tracking.py
@@ -54,30 +54,19 @@ connection.execute(text('ALTER TABLE covid_2 RENAME TO covid'))
 
 #df = pd.read_sql_table('covid', connection)
 
-months = ('Mar 2020', 'Apr 2020', 'May 2020', 'Jun 2020', 'Jul 2020', 'Aug 2020', 'Sep 2020', 'Oct 2020', 'Nov 2020', 'Dec 2020', 'Jan 2021', 'Feb 2021', 'March 2021')
-patients_cat = {
-    'Hospitalized': (32.221, 784.533, 2561.872, 4038.095, 5535.849, 7723.933, 8799.572, 10414.102, 12283.586, 16262.903, 20134.520, 20714.612, 5405.727),
-    'Deaths': (20.739, 881.423, 2587.987, 335.6899, 4071.555, 5012.258, 5648.154, 6528.270, 7206.395, 9269.261, 11931.561, 13240.568, 3571.866),
-}
+df = pd.read_csv('https://api.covidtracking.com/v1/us/daily.csv')
 
-x = np.arange(len(months))  # the label locations
-width = 0.45  # the width of the bars
-multiplier = 0
 
-fig, ax = plt.subplots(layout='constrained')
+df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
 
-for attribute, measurement in patients_cat.items():
-    offset = width * multiplier
-    rects = ax.bar(x + offset, measurement, width, label=attribute)
-    ax.bar_label(rects, padding=2)
-    multiplier += 1
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Number of Patients')
-ax.set_title('Hospitalized & Death: The end of Pandemic CoV19')
-ax.set_xticks(x + width, months)
-ax.legend(loc='upper left', ncols=3)
-ax.set_ylim(10, 21000)
+fig, ax = plt.subplots()
+ax.plot(df['date'], df['death'], label='Deaths')
+ax.plot(df['date'], df['hospitalized'], label='Hospitalizations')
+ax.set_xlabel('Date')
+ax.set_ylabel('Number of People')
+ax.set_title('Deaths and Hospitalizations Due to COVID-19 in the United States')
+ax.legend()
+
 plt.show()
-
 connection.close
